9dfb34fec2198a3d85d9d09795e546780295f69a
Hugo Nogueira <hnogueira@marabyte.com>
2022-08-29 19:42:20 +0100

    feat: Add isDarkColour (#35)

---
+++ src/isDarkColour.ts
@@ -0,0 +1,28 @@
+import type { NamedCSSColour, WCAG } from "./types";
+import { isHexDarkColour } from "./isHexDarkColour.js";
+import { isCSSRGBDarkColour } from "./isCSSRGBDarkColour.js";
+import { isCSSNamedDarkColour } from "./isCSSNameDarkColour.js";
+
+/**
+ * Evaluates if a colour is dark by measuring the contrast ratio against black and white.
+ * It accepts CSS RGB colours, named CSS or hexadecimal.
+ * 
+ * If you know in advance the type of colour you want to evaluate consider using `isCSSNamedDarkColour`, `isCSSRGBDarkColour` or `isHexDarkColour` as they are smaller functions
+ * @param {string | NamedCSSColour} colour - A colour value, ie: `hotpink`, `#333222` or `rgb(12,34,45)` 
+ * @param {"WCAG2.1" | "WCAG3.0"} standard - Evaluate agains "WCAG2.1" or "WCAG3.0"
+ * @returns {boolean} Returns `true`, `false` or `undefined` if name is not a valid CSS named colour
+ */
+export function isDarkColour(colour: string | NamedCSSColour, standard: WCAG): boolean {
+    try {
+        if (colour.startsWith('#')) {
+            return isHexDarkColour(colour, standard);
+        }
+        if (colour.startsWith('rgb')) {
+            return isCSSRGBDarkColour(colour, standard);
+        }
+        return isCSSNamedDarkColour(colour as NamedCSSColour, standard);
+    } catch (error) {
+        throw new Error(`${colour} is not a valid colour format. isDarkColour accepts CSS RGB formats, ie rgb(0,0,0) and rgba(255, 255, 255, 0.4), hexadecimal and CSS named colours.`)
+
+    }
+}
---
+++ src/tests/isDarkColour.test.ts
@@ -0,0 +1,38 @@
+import test from "ava";
+import { isDarkColour } from "../isDarkColour.js";
+
+test("should return true for `darkblue`", ({ is }) => {
+    is(isDarkColour("darkblue", "WCAG2.1"), true);
+});
+
+test("should return false for `lightgoldenrodyellow`", ({ is }) => {
+    is(isDarkColour("lightgoldenrodyellow", "WCAG2.1"), false);
+});
+
+test("should return true for `rgb(20, 20, 20)`", ({ is }) => {
+    is(isDarkColour("rgb(20, 20, 20)", "WCAG2.1"), true);
+});
+
+test("should return false for `rgb(200, 200, 200)`", ({ is }) => {
+    is(isDarkColour("rgb(200, 200, 200)", "WCAG2.1"), false);
+});
+
+test("should return false for #BED colour", ({ is }) => {
+    is(isDarkColour("#BED", "WCAG2.1"), false);
+});
+
+test("should return true for #666666 colour", ({ is }) => {
+    is(isDarkColour("#666666", "WCAG2.1"), true);
+});
+
+test("throws an error if not passing a valid CSS RGB format", ({
+    is,
+    throws,
+}) => {
+    const rgb = "rfv(12,23,42)";
+    const error = throws(() => isDarkColour(rgb, "WCAG2.1")) as Error;
+    is(
+        error.message,
+        "rfv(12,23,42) is not a valid colour format. isDarkColour accepts CSS RGB formats, ie rgb(0,0,0) and rgba(255, 255, 255, 0.4), hexadecimal and CSS named colours."
+    );
+});
